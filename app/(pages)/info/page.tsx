26bb870d6d278b27152e6dcf87d49a6b41c67f3a
Vladislav Bakhteev <vladislav.bakhteev@cresta.ai>
2024-03-14 20:17:34 +0100

    Add info page

--- app/(pages)/info/page.tsx
+++ app/(pages)/info/page.tsx
@@ -1,9 +1,58 @@
+"use client"
 
+import { Button } from '@/components/ui/button';
+import { Mic, SendHorizonal } from 'lucide-react';
+import { RusKeyboard } from './_components/rus-keyboard';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { Input } from '@/components/ui/input';
+import { useState } from 'react';
 
 export default function Home() {
+  const router = useRouter();
+
+  const searchParams = useSearchParams();
+  const question = searchParams.get('question');
+  if (question) {
+    // TODO: add logic to handle question with LLM and redirect to the appropriate page
+  }
+
+  const [value, setValue] = useState('')
+
+  const onSubmit = () => {
+    const params = new URLSearchParams({'question': value });
+    router.push(`/info?${params.toString()}`);
+  }
+
   return (
-    <div>
-        <h1>Info</h1>
+    <div className=''>
+        <div className='group absolute bottom-0 w-full flex flex-col'>
+          <div className='flex px-6 mb-2'>
+            <Button
+             variant="secondary"
+             size="icon"
+             onClick={() => {}}
+             className='rounded-full bg-white border-slate-300 border p-2 mr-2'
+            >
+              <Mic />
+            </Button>
+            <Input
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+              // className='group-focus'
+            />
+            <Button
+             variant="secondary"
+             size="icon"
+             onClick={onSubmit}
+             className='rounded-full bg-blue-300 text-white border-slate-400 border p-2 ml-2'
+            >
+              <SendHorizonal />
+            </Button>
+          </div>
+          <RusKeyboard
+            onChange={(input: string) => setValue(input)}
+          />
+        </div>
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import { VisualButton } from "@/components/visual-button";
-import { Soup } from "lucide-react";
 import { Menu } from "./_components/menu";
 
 export default function Home() {
